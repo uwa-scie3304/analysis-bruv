f516f29c8802267963e151ee6e8183a6be20c0cb
claudehspencer <claudespencer111@gmail.com>
2024-05-03 09:00:47 +0800

    working on data analysis

--- r/gam model/02_importance plots.R
+++ r/gam model/02_importance plots.R
@@ -24,12 +24,11 @@ name <- "Parks-Ningaloo-synthesis"
 # Read in the data
 dat <- read.csv(paste("model out", paste(name, "all.var.imp.csv", sep = "_"), sep = "/")) %>% 
   dplyr::rename(resp.var = X) %>%
-  pivot_longer(cols = c(eveyrthing but resp.vars), names_to = "predictor", values_to = "importance") %>%
-  # gather(key = predictor, value = importance,2:ncol(.)) %>%
+  pivot_longer(cols = -c(resp.var), names_to = "predictor", values_to = "importance") %>%
   glimpse()
 
 dat.taxa <- dat %>%
-  dplyr::mutate(label = case_when(predictor %in% "" & resp.var %in% "", "X",
+  dplyr::mutate(label = case_when(predictor %in% "" & resp.var %in% "" ~ "X",
                                   .default = NA)) %>%
   glimpse()
 
@@ -37,24 +36,13 @@ dat.taxa <- dat %>%
 re <- colorRampPalette(c("blue3", "white","red2"))(200)
 
 # Labels-
-legend_title<-"Importance"
+legend_title <- "Importance"
 
-imp.full <- ggplot(dat.taxa %>% dplyr::filter(resp.var%in%c("total.abundance", "species.richness")), 
-                   aes(x=predictor,y=resp.var,fill=importance)) +
-  geom_tile(show.legend=T) +
-  scale_fill_gradientn(legend_title, colours=c(re), na.value = "grey98",
-                       limits = c(-1, 1))+
-  scale_y_discrete(labels=c("Species richness","Total abundance"))+
-  labs(x = NULL, y = NULL, title = "Whole assemblage") +
-  theme_classic()+
-  geom_text(aes(label=label)) +
-  theme(axis.title.x=element_blank(),
-        axis.text.x=element_blank(),
-        axis.ticks.x=element_blank(), 
-        axis.line.x = element_blank(),
-        plot.title = element_text(hjust = -0.45, vjust = -15)) # Looks crap here but title comes back in exported version
-imp.full
+ggplot(dat.taxa, aes(x = predictor, y = resp.var, fill = importance)) +
+  geom_tile(show.legend = T) +
+  scale_fill_gradient(low = "white", high = "red2", limits = c(0, 1), name = "Importance") +
+  labs(x = NULL, y = NULL) +
+  theme_classic()
 
 #save output - changed dimensions for larger text in report
-save_plot(paste0("figures/fish/", name, "_importance-scores.png"), 
-          gg.importance,base_height = 4,base_width = 6.275)
+ggsave(paste0("figures/fish/", name, "_importance-scores.png"),height = 4, width = 6.275, res = 300)
